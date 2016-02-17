6199db299ec468ea740b6b6fdf2a348be3328150
kylemorton5770 <michael.morton@smail.astate.edu>
2016-02-17 15:52:17 -0600

    added first views and connected to local mongo db instance

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -1,17 +1,10 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: function () {
+      return Tasks.find({});
     }
   });
 }
