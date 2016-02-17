49a3da58c848d12ba33ff86792887563e47d6bdd
kylemorton5770 <michael.morton@smail.astate.edu>
2016-02-17 16:07:14 -0600

    added check and remove functionality

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -4,9 +4,46 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({});
+      //sort by newest tasks
+      return Tasks.find({}, {sort: {createdAt: -1}});
     }
   });
+  
+  Template.body.events({
+    "submit .new-task": function(event){
+      //prevent default browser form submit
+      event.preventDefault();
+      
+      console.log("Event: " + event);
+      
+      //get value from form element
+      var text = event.target.text.value;
+      
+      //insert task into MDB collection
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+      
+      //clear form
+      event.target.text.value = "";
+      
+    }
+  });
+  
+  Template.task.events({
+    "click .toggle-checked": function(){
+      //set the checked property to the opposite of its current value
+      Tasks.update(this._id, {
+        $set: {checked: !this.checked}
+      });
+    },
+    "click .delete": function(){
+      Tasks.remove(this._id);
+    }
+  });
+  
+  
 }
 
 if (Meteor.isServer) {
