e0b4d878697bd63a429e3b89874e85dd2218ca97
kylemorton5770 <michael.morton@smail.astate.edu>
2016-02-17 16:16:13 -0600

    added hiding completed tasks and remaining tasks counter

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -4,8 +4,19 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      //sort by newest tasks
-      return Tasks.find({}, {sort: {createdAt: -1}});
+      if (Session.get("hideCompleted")) {
+        // If hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+    },
+    incompleteCount: function(){
+      return Tasks.find({checked: {$ne: true}}).count();
     }
   });
   
@@ -26,8 +37,10 @@ if (Meteor.isClient) {
       });
       
       //clear form
-      event.target.text.value = "";
-      
+      event.target.text.value = ""; 
+    },
+    "change .hide-completed input": function(event){
+      Session.set("hideCompleted", event.target.checked);
     }
   });
   
