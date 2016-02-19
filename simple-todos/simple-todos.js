0bd32ea29610c65fcfbce871948e7e57c68363aa
kylemorton5770 <michael.morton@smail.astate.edu>
2016-02-19 14:16:50 -0600

    added security to Meteor custom methods to allow only task-owner to delete task

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -1,6 +1,5 @@
 Tasks = new Mongo.Collection("tasks");
 
-
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
@@ -103,9 +102,19 @@ Meteor.methods({
       });
   },
   deleteTask: function(taskId){
+    var task = Tasks.findOne(taskId);
+    if (task.owner !== Meteor.userId()){
+      // If the task is private, make sure only the owner can delete it
+      throw new Meteor.Error("not-authorized");
+    }
     Tasks.remove(taskId);
   },
   setChecked: function (taskId, setChecked) {
+    var task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      // If the task is private, make sure only the owner can check it off
+      throw new Meteor.Error("not-authorized");
+    }
     Tasks.update(taskId, {$set: {checked: setChecked}}); 
   },
   setPrivate: function (taskId, setToPrivate) {
